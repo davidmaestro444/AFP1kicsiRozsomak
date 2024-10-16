08b52127403ee1d5d8fbaeff607b1695a44e7c44
B. Péter <165163343+BgtPtr@users.noreply.github.com>
2024-10-16 21:14:41 +0200

    Menu created

---
+++ AFP1kicsiRozsomak/projekt/projekt/projekt/User_Menu/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_MouseHover(object sender, EventArgs e)
+        {
+            btn_start.Image = Properties.Resources.start_hover;
+        }
+
+        private void btn_exit_MouseHover(object sender, EventArgs e)
+        {
+            btn_exit.Image = Properties.Resources.exit_hover;
+        }
+
+        private void btn_start_MouseLeave(object sender, EventArgs e)
+        {
+            btn_start.Image= Properties.Resources.start_normal;
+        }
+
+        private void btn_exit_MouseLeave(object sender, EventArgs e)
+        {
+            btn_exit.Image = Properties.Resources.exit_normal;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            menu.Hide();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
